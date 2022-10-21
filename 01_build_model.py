d9333606fb9d3f056750bfd794c204ce0340d521
Marcin Żebrowski <74146420+Zuberoman@users.noreply.github.com>
2022-10-21 12:17:17 +0200

    Update 01_build_model.py

--- 01_build_model.py
+++ 01_build_model.py
@@ -6,6 +6,10 @@ Created on Sun Mar 14 15:22:51 2021
 """
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 np.random.seed(10)
 #%%
@@ -34,17 +38,14 @@ df.to_csv('data_cleaned.csv')
 X=df.copy()
 y=X.pop('Price')
 #%%
-from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y)
 #%%
-from sklearn.ensemble import RandomForestRegressor
 reg=RandomForestRegressor()
 reg.fit(X_train,y_train)
 
 print(reg.score(X_test,y_test))
 #%%
 
-from sklearn.model_selection import GridSearchCV
 model=RandomForestRegressor()
 param_grid=[{'max_depth':[3,4,5,6,7,8,10,20],
              'min_samples_leaf':[3,4,5,10,15]}]
@@ -55,25 +56,7 @@ print(gd.score(X_test,y_test))
 #%%
 model=gd.best_estimator_
 #%%
-import pickle
 
 with open('model.pickle','wb') as file:
     pickle.dump(model,file)
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
